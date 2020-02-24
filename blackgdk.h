adc6463ab440b500a2259df2ca2b312e0a651f5a
Popov Evgeniy <tuzik87@inbox.ru>
2020-02-24 15:23:57 +0300

    Source code was synced with Neon game development kit 4.2.8

--- blackgdk.h
+++ blackgdk.h
@@ -195,10 +195,11 @@ class Frame
  void set_size(const SURFACE surface);
  void create_buffers();
  unsigned long int get_frame_line();
- unsigned int *get_buffer();
  public:
  Frame();
  ~Frame();
+ unsigned int *get_buffer();
+ size_t get_pixels();
  void draw_pixel(const unsigned long int x,const unsigned long int y,const unsigned char red,const unsigned char green,const unsigned char blue);
  void clear_screen();
  void save();
