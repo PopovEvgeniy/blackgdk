6f4373d05430a015c8accdb332e4a8fd5368ba84
Popov Evgeniy <tuzik87@inbox.ru>
2020-05-14 13:52:34 +0700

    Source code was synced with Neon game development kit 4.6.7

--- blackgdk.cpp
+++ blackgdk.cpp
@@ -1982,9 +1982,9 @@ Canvas::~Canvas()
 
 void Canvas::set_frame(const unsigned long int target)
 {
- if (target<=frames)
+ if (target>0)
  {
-  frame=target;
+  if (target<=frames) frame=target;
  }
 
 }
