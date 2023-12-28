378f8bd3e8a5588b8abd69a0bdef20ca79cf4b4c
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2023-12-28 23:51:10 +0300

    Source code synced with Eugene game development kit 7.8.4

--- source/blackgdk.cpp
+++ source/blackgdk.cpp
@@ -3633,6 +3633,8 @@ namespace BLACKGDK
 
   World::World()
   {
+   surface_width=1;
+   surface_height=1;
    half_tile_width=1;
    half_tile_height=1;
   }
@@ -3642,7 +3644,7 @@ namespace BLACKGDK
 
   }
 
-  void World::initialize(const int tile_width,const int tile_height)
+  void World::initialize(const int tile_width,const int tile_height,const int screen_width,const int screen_height)
   {
    if (tile_width>1)
    {
@@ -3651,6 +3653,14 @@ namespace BLACKGDK
    if (tile_height>1)
    {
     half_tile_height=tile_height/2;
+   }
+   if (screen_width>1)
+   {
+    surface_width=screen_width;
+   }
+   if (screen_height>1)
+   {
+    surface_height=screen_height;
    }
 
   }
@@ -3685,6 +3695,33 @@ namespace BLACKGDK
     column/=2;
    }
    return column;
+  }
+
+  int World::get_row_amount() const
+  {
+   int amount;
+   amount=surface_width/half_tile_width;
+   if ((surface_width%half_tile_width)!=0)
+   {
+    ++amount;
+   }
+   return amount;
+  }
+
+  int World::get_column_amount() const
+  {
+   int amount;
+   amount=surface_height/half_tile_height;
+   if ((surface_height%half_tile_height)!=0)
+   {
+    ++amount;
+   }
+   return amount;
+  }
+
+  int World::get_tile_amount() const
+  {
+   return this->get_row_amount()*this->get_column_amount();
   }
 
  }
