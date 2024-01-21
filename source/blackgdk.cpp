6175bba6606729d96c878dd884c537b1d269d683
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2024-01-22 00:39:10 +0300

    Source code synced with Eugene game development kit 7.9.6

--- source/blackgdk.cpp
+++ source/blackgdk.cpp
@@ -2052,6 +2052,11 @@ namespace BLACKGDK
 
   }
 
+  Input_File* Input_File::get_handle()
+  {
+   return this;
+  }
+
   void Input_File::open(const char *name)
   {
    this->close();
@@ -2079,6 +2084,11 @@ namespace BLACKGDK
   Output_File::~Output_File()
   {
 
+  }
+
+  Output_File* Output_File::get_handle()
+  {
+   return this;
   }
 
   void Output_File::open(const char *name)
