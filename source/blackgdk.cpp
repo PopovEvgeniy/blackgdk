b27c817d8e07209871844083383289b9ee98e73d
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2024-05-20 02:23:52 +0700

    Source code synced with Eugene game development kit 8.7.3

--- source/blackgdk.cpp
+++ source/blackgdk.cpp
@@ -292,11 +292,6 @@ namespace BLACKGDK
     CloseWindow(window);
     window=NULL;
    }
-   if (window_class.hbrBackground!=NULL)
-   {
-    DeleteObject(window_class.hbrBackground);
-    window_class.hbrBackground=NULL;
-   }
    UnregisterClass(window_class.lpszClassName,window_class.hInstance);
   }
 
