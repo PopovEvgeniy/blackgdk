b22d743dc4a09931698c415628622b13d9c07c31
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2025-03-01 22:21:39 +0700

    The source code synced with the Eugene game development kit 9.5.6

--- source/blackgdk.cpp
+++ source/blackgdk.cpp
@@ -2783,6 +2783,24 @@ namespace BLACKGDK
 
   }
 
+  unsigned int Billboard::increase_x(const unsigned int increment)
+  {
+   if (billboard.is_texture_exist()==true)
+   {
+    current_x+=increment;
+   }
+   return current_x;
+  }
+
+  unsigned int Billboard::increase_y(const unsigned int increment)
+  {
+   if (billboard.is_texture_exist()==true)
+   {
+    current_y+=increment;
+   }
+   return current_y;
+  }
+
   unsigned int Billboard::decrease_x(const unsigned int decrement)
   {
    if (billboard.is_texture_exist()==true)
@@ -2809,67 +2827,25 @@ namespace BLACKGDK
    return current_y;
   }
 
-  unsigned int Billboard::decrease_x()
-  {
-   if (billboard.is_texture_exist()==true)
-   {
-    if (current_x>0)
-    {
-     --current_x;
-    }
-
-   }
-   return current_x;
-  }
-
-  unsigned int Billboard::decrease_y()
-  {
-   if (billboard.is_texture_exist()==true)
-   {
-    if (current_y>0)
-    {
-     --current_y;
-    }
-
-   }
-   return current_y;
-  }
-
-  unsigned int Billboard::increase_x(const unsigned int increment)
-  {
-   if (billboard.is_texture_exist()==true)
-   {
-    current_x+=increment;
-   }
-   return current_x;
-  }
-
-  unsigned int Billboard::increase_y(const unsigned int increment)
-  {
-   if (billboard.is_texture_exist()==true)
-   {
-    current_y+=increment;
-   }
-   return current_y;
-  }
-
   unsigned int Billboard::increase_x()
   {
-   if (billboard.is_texture_exist()==true)
-   {
-    ++current_x;
-   }
-   return current_x;
+   return this->increase_x(1);
   }
 
 
   unsigned int Billboard::increase_y()
   {
-   if (billboard.is_texture_exist()==true)
-   {
-    ++current_y;
-   }
-   return current_y;
+   return this->increase_y(1);
+  }
+
+  unsigned int Billboard::decrease_x()
+  {
+   return this->decrease_x(1);
+  }
+
+  unsigned int Billboard::decrease_y()
+  {
+   return this->decrease_y(1);
   }
 
   unsigned int Billboard::get_start_x() const
