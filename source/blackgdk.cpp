7e57038a81682354d3aa3a2c87fc00e935c6cca5
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2025-11-08 21:43:00 +0700

    The source code synced with the Eugene Game Development Kit 9.8.6

--- source/blackgdk.cpp
+++ source/blackgdk.cpp
@@ -2214,7 +2214,7 @@ namespace BLACKGDK
 
   }
 
-  bool Screen::update()
+  bool Screen::sync(const bool limit)
   {
    if (this->get_context()!=NULL)
    {
@@ -2222,22 +2222,21 @@ namespace BLACKGDK
     this->update_counter();
     this->clear_stage();
    }
+   if (limit==true)
+   {
+    this->wait_timer();
+   }
    return this->process_message();
   }
 
   bool Screen::sync()
   {
-   this->wait_timer();
-   return this->update();
+   return this->sync(true);
   }
 
-  bool Screen::sync(const bool limit)
+  bool Screen::update()
   {
-   if (limit==true)
-   {
-    this->wait_timer();
-   }
-   return this->update();
+   return this->sync(false);
   }
 
   bool Screen::is_ready()
