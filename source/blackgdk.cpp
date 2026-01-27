466db0e0699b656bdc83a1fdf18d4819c2443926
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2026-01-28 03:15:56 +0700

    The source code synced with the Eugene Game Development Kit 10.2.2

--- source/blackgdk.cpp
+++ source/blackgdk.cpp
@@ -3792,7 +3792,7 @@ namespace BLACKGDK
       columns=column_amount;
       this->reset_animation_settings();
       this->set_frames(rows*columns);
-      this->set_target(1);
+      this->select(1,1);
       this->prepare_sheet();
      }
 
